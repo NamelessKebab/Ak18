660f9394c96b7c22be7006ef6e63bf97df40611d
Julian <julian.blazek2000@gmail.com>
2017-03-17 23:46:45 +0100

    PlayerPhysics.java dokumentiert :pencil:

--- src/platformer/physics/PlayerPhysics.java
+++ src/platformer/physics/PlayerPhysics.java
@@ -8,25 +8,51 @@ package platformer.physics;
 import platformer.core.Player;
 
 /**
- *
+ * PlayerPhysics.java
+ * Zweck: Steuert die Physik eines
+ * Player Objekts
+ * 
  * @author Julian Blazek
+ * @version 1.0 17.03.2017
  */
 public class PlayerPhysics {
-    
-    public static void update(platformer.core.Player player, double ortsfaktor, double deltaTime){
+
+    /**
+     * Diese Methode berechnet die Physik für den gegebenen Zeitraum.
+     * ySpeed basiert auf v=g*dt wobei v = ySpeed; g = gravity, t = deltaTime.
+     * y basiert auf s=v*t wobei s = y0+y1; v = ySpeed; t = deltaTime.
+     * 
+     * Wäre der Player nun nach dem berechnen der Physik unterhalb des Bildschirm
+     * Randes, wird er wieder auf die vorherige y-Position zurückgesetzt und
+     * die Berechnung wird erneut ausgeführt.
+     * 
+     * @param player Player Objekt
+     * @param gravity Die Stärke der Gravität mit der gerechnet werden soll. (default ~100)
+     * @param deltaTime Die Zeit für die die Physik berechnet werden soll
+     */
+    public static void update(platformer.core.Player player, double gravity, double deltaTime){
         int alty,y;
         double ySpeed;
         alty = player.getY();
-        ySpeed = player.getySpeed() + ortsfaktor * deltaTime;
+        ySpeed = player.getySpeed() + gravity * deltaTime;
         player.setySpeed(ySpeed);
         y = (int) (player.getY() + player.getySpeed() * deltaTime);
         player.setY(y);
         if(y > 550) player.setY(alty);
         System.out.println("Y: "+y+" ySpeed: "+ySpeed);
     }
-
-    public static void springe(Player play) {
-        play.setySpeed(-100);
+    
+    /**
+     * Setzt die y-Geschwindigkeit auf einen
+     * negativen Wert. Oben links im Koordinaten-
+     * system ist 0. Nach rechts steigt x an,
+     * nach links y. Daher ist die y-Geschwindigkeit
+     * negativ.
+     * 
+     * @param player Player Objekt
+     */
+    public static void springe(Player player) {
+        player.setySpeed(-100);
     }
     
 }
